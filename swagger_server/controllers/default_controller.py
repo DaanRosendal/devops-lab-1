444db1f45e9ddaeb7d4322e0a2fca572188f6ffc
Daan Rosendal <daan.rosendal@cern.ch>
2025-02-06 09:44:40 +0100

    Refactor default_controller to use student_service methods
    - Remove unused imports
    - Replace placeholder 'do some magic!' with actual service method calls
    - Add error handling for non-JSON requests
    - Integrate add(), get_by_id(), and delete() methods from student_service

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -6,6 +6,7 @@ from swagger_server.models.inline_response400 import InlineResponse400  # noqa: 
 from swagger_server.models.inline_response404 import InlineResponse404  # noqa: E501
 from swagger_server.models.student import Student  # noqa: E501
 from swagger_server import util
+from swagger_server.service.student_service import add, get_by_id, delete
 
 
 def add_student(body):  # noqa: E501
@@ -20,7 +21,8 @@ def add_student(body):  # noqa: E501
     """
     if connexion.request.is_json:
         body = Student.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        return add(body)
+    return 500, "Internal Server Error"
 
 
 def delete_student(student_id):  # noqa: E501
@@ -33,7 +35,7 @@ def delete_student(student_id):  # noqa: E501
 
     :rtype: InlineResponse200
     """
-    return 'do some magic!'
+    return delete(student_id)
 
 
 def get_student_by_id(student_id):  # noqa: E501
@@ -46,4 +48,4 @@ def get_student_by_id(student_id):  # noqa: E501
 
     :rtype: Student
     """
-    return 'do some magic!'
+    return get_by_id(student_id)
